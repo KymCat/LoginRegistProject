ce977500aa81d6aba2c3f971ae91ccb49547c109
KymCat <kohina1452@gmail.com>
2025-03-07 15:52:44 +0900

    로그인 인증 실패 핸들러 등록

---
+++ src/main/java/com/example/loginProject/handler/CustomAuthenticationFailureHandler.java
@@ -0,0 +1,23 @@
+package com.example.loginProject.handler;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Slf4j
+@Component
+public class CustomAuthenticationFailureHandler implements AuthenticationFailureHandler {
+    @Override
+    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception)
+            throws IOException, ServletException {
+        log.info("Login Fail");
+        request.getSession().setAttribute("message", "아이디 또는 비밀번호가 잘못되었습니다.");
+        response.sendRedirect("/auth");
+    }
+}
